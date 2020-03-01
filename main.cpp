6079e7b9050b66af5b2218f9acf4309130cb9bae
faseehahmad00@gmail.com <FASEEH21100@f>
2020-03-01 19:49:19 +0500

    bugs fixed.

--- main.cpp
+++ main.cpp
@@ -9,29 +9,12 @@ int main() {
     char array[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     int counter = 0;
     int choice;
-    Game::drawboard(array);
+    //Game::drawboard(array);
     while (true) {
-        cout << "\n\nYour turn.\nPlease select an option : ";
-        cin >> choice;               //players choice
-        if (choice > 0 && choice < 10) {
-            if (array[choice - 1] != 'x' && array[choice - 1] != 'o') {
-                array[choice - 1] = 'o';
-                counter++;
-            } else {
-                cout << "\nInvalid option,try again";
-                continue;
-            }
-        } else {
-            cout << "\nInvalid option,try again";
-            continue;
-        }
-        system("clear");
-        Game::drawboard(array);
-        if (Game::checkwin(array, counter)) {
-            return 0;
-        }
+
         //Computer's Turn
         while (true) {
+            srand(time(nullptr));
             int num = rand() % 9;
             if (array[num] != 'o' && array[num] != 'x') {
                 array[num] = 'x';
@@ -44,5 +27,25 @@ int main() {
         if (Game::checkwin(array, counter)) {
             return 0;
         }
+        a:  cout << "\n\nYour turn.\nPlease select an option : ";
+        cin >> choice;               //players choice
+        if (choice > 0 && choice < 10) {
+            if (array[choice - 1] != 'x' && array[choice - 1] != 'o') {
+                array[choice - 1] = 'o';
+                counter++;
+            } else {
+                cout << "\nInvalid option,try again\n";
+                goto a;
+            }
+        } else {
+            cout << "\nInvalid option,try again\n";
+            goto a;}
+        system("clear");
+        Game::drawboard(array);
+        if (Game::checkwin(array, counter)) {
+            return 0;
+        }
+
     }
 }
+
