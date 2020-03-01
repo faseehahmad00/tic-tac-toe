a5efe5c0163aa739f1e38d4020d2566482dd1432
faseehahmad00@gmail.com <FASEEH21100@f>
2020-03-01 14:18:24 +0500

    added random function

--- main.cpp
+++ main.cpp
@@ -8,23 +8,30 @@ int main() {
     system("clear");
     char array[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     int counter = 0;
-    char choice;
+    int choice;
     Game::drawboard(array);
-    while(true) {
+    while (true) {
         cout << "\n\nPlease select an option : ";
-        cin >> choice;
-        for (int i = 0; i <= 9; i++) {                   //players choice
-            if (choice == array[i]) {
-                array[i] = 'o';
+        cin >> choice;               //players choice
+        if (choice > 0 && choice < 10) {
+            if (array[choice - 1] != 'x' && array[choice - 1] != 'o') {
+                array[choice - 1] = 'o';
                 counter++;
+            } else {
+                cout << "\nInvalid option,try again";
+                continue;
             }
+
+        } else {
+            cout << "\nInvalid option,try again";
+            continue;
         }
         system("clear");
         Game::drawboard(array);
         if (Game::checkwin(array, counter)) { return 0; }  //checking win cases
         //COMPUTERS TURN
         while (true) {
-            int num = rand() % 8;
+            int num = rand() % 8 +1;
             if (array[num] != 'o' && array[num] != 'x') {
                 array[num] = 'x';
                 counter++;
@@ -34,6 +41,7 @@ int main() {
         system("clear");
         Game::drawboard(array);
         if (Game::checkwin(array, counter)) {
-            return 0; }   //check win cases
+            return 0;
+        }   //check win cases
     }
 }
