97e4571da27691645db41952fbdf0c1a15ee264e
faseehahmad00@gmail.com <FASEEH21100@f>
2020-03-01 14:45:12 +0500

    added random function

--- main.cpp
+++ main.cpp
@@ -9,9 +9,22 @@ int main() {
     char array[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     int counter = 0;
     int choice;
-    Game::drawboard(array);
     while (true) {
-        cout << "\n\nPlease select an option : ";
+        //Computer's Turn
+        while (true) {
+            int num = rand() % 8 + 1;
+            if (array[num] != 'o' && array[num] != 'x') {
+                array[num] = 'x';
+                counter++;
+                break;
+            }
+        }
+        system("clear");
+        Game::drawboard(array);   //check win cases
+        if (Game::checkwin(array, counter)) {
+            return 0;
+        }
+        cout << "\n\nYour turn.\nPlease select an option : ";
         cin >> choice;               //players choice
         if (choice > 0 && choice < 10) {
             if (array[choice - 1] != 'x' && array[choice - 1] != 'o') {
@@ -21,27 +34,11 @@ int main() {
                 cout << "\nInvalid option,try again";
                 continue;
             }
-
         } else {
             cout << "\nInvalid option,try again";
             continue;
         }
         system("clear");
         Game::drawboard(array);
-        if (Game::checkwin(array, counter)) { return 0; }  //checking win cases
-        //COMPUTERS TURN
-        while (true) {
-            int num = rand() % 8 +1;
-            if (array[num] != 'o' && array[num] != 'x') {
-                array[num] = 'x';
-                counter++;
-                break;
-            }
-        }
-        system("clear");
-        Game::drawboard(array);
-        if (Game::checkwin(array, counter)) {
-            return 0;
-        }   //check win cases
     }
 }
