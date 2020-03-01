82d4e8f9fbd9ee3dded8285c168d853f6ebca857
faseehahmad00@gmail.com <FASEEH21100@f>
2020-03-01 14:00:53 +0500

    added random function

--- main.cpp
+++ main.cpp
@@ -10,29 +10,30 @@ int main() {
     int counter = 0;
     char choice;
     Game::drawboard(array);
-    a:
-    cout << "\n\nPlease select an option : ";
-    cin >> choice;
-    for (int i = 0; i <= 9; i++) {                   //players choice
-        if (choice == array[i]) {
-            array[i] = 'o';
-            counter++;
+    while(true) {
+        cout << "\n\nPlease select an option : ";
+        cin >> choice;
+        for (int i = 0; i <= 9; i++) {                   //players choice
+            if (choice == array[i]) {
+                array[i] = 'o';
+                counter++;
+            }
         }
-    }
-    system("clear");
-    Game::drawboard(array);
-    if (Game::checkwin(array, counter)) { return 0; }  //checking win cases
-    //COMPUTERS TURN
-    while (true) {
-        int num = rand() % 8;
-        if (array[num] != 'o' && array[num] != 'x') {
-            array[num] = 'x';
-            counter++;
-            break;
+        system("clear");
+        Game::drawboard(array);
+        if (Game::checkwin(array, counter)) { return 0; }  //checking win cases
+        //COMPUTERS TURN
+        while (true) {
+            int num = rand() % 8;
+            if (array[num] != 'o' && array[num] != 'x') {
+                array[num] = 'x';
+                counter++;
+                break;
+            }
         }
+        system("clear");
+        Game::drawboard(array);
+        if (Game::checkwin(array, counter)) {
+            return 0; }   //check win cases
     }
-    system("clear");
-    Game::drawboard(array);
-    if (Game::checkwin(array, counter)) { return 0; }   //check win cases
-    else { goto a; }
 }
